6d7220736e43de5a744a7acd623885d2d38704af
Nigro Simone <nigro.simone@gmail.com>
2021-03-28 18:08:35 +0200

    up

--- projects/ng-condition/src/lib/ng-condition.component.spec.ts
+++ projects/ng-condition/src/lib/ng-condition.component.spec.ts
@@ -1,5 +1,7 @@
 import { Component, DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgConditionBlock } from './ng-condition-block';
+import { NgConditionComponent } from './ng-condition.component';
 import { NgConditionModule } from './ng-condition.module';
 
 
@@ -69,4 +71,42 @@ describe('NgCondition', () => {
         fixture.detectChanges();
         expect(element.textContent.trim()).toBe('Number is major of 20');
     });
+});
+
+class MockNgConditionBlock extends NgConditionBlock {}
+class MockQueryList {
+    constructor(private array: Array<MockNgConditionBlock>){}
+    toArray(): Array<MockNgConditionBlock> {
+        return this.array;
+    }
+}
+
+describe('NgCondition error', () => {
+
+
+    it('test if 1', () => {
+        const comp = new NgConditionComponent();
+        comp.ngElseIfComponents = new MockQueryList([new MockNgConditionBlock()]) as any;
+        expect( function(){ comp.ngAfterContentChecked(); } ).toThrow(new Error(`One <ng-if></ng-if> is mandatory for <ng-condition>`));
+    });
+
+    it('test if 2', () => {
+        const comp = new NgConditionComponent();
+        comp.ngElseComponents = new MockQueryList([new MockNgConditionBlock()]) as any;
+        expect( function(){ comp.ngAfterContentChecked(); } ).toThrow(new Error(`One <ng-if></ng-if> is mandatory for <ng-condition>`));
+    });
+
+    it('test multiple if', () => {
+        const comp = new NgConditionComponent();
+        comp.ngIfComponents = new MockQueryList([new MockNgConditionBlock(), new MockNgConditionBlock()]) as any;
+        expect( function(){ comp.ngAfterContentChecked(); } ).toThrow(new Error(`Only one <ng-if></ng-if> is allowed for <ng-condition>`));
+    });
+
+    it('test multiple else', () => {
+        const comp = new NgConditionComponent();
+        comp.ngIfComponents = new MockQueryList([new MockNgConditionBlock()]) as any;
+        comp.ngElseComponents = new MockQueryList([new MockNgConditionBlock(), new MockNgConditionBlock()]) as any;
+        expect( function(){ comp.ngAfterContentChecked(); } ).toThrow(new Error(`Only one <ng-else></ng-else> is allowed for <ng-condition>`));
+    });
+
 });
